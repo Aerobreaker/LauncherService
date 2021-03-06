e3d1ea622cb7d9192224b369966040faf74520e7
Aerobreaker <44306739+Aerobreaker@users.noreply.github.com>
2021-03-06 16:12:06 -0700

    Update ServiceLauncher.cs
    I forgot to save the comments I added in main.

--- ServiceLauncher/ServiceLauncher.cs
+++ ServiceLauncher/ServiceLauncher.cs
@@ -5,7 +5,7 @@
  * 1. Open named pipes for foreground communication
  * 2. Forward input and output from child using named pipes
  * 3. Use pid of child to detect opened ports (optional)
- * 4. Open ports via upnp (either via step 5 or input parameters)
+ * 4. Open ports via upnp (either via step 5 or input parameters) (see Device Host API)
  */
 
 namespace ServiceLauncher
@@ -14,14 +14,17 @@ namespace ServiceLauncher
     {
         static void Main(string[] args)
         {
+            //Instantiate new child
             ChildProc child = new ChildProc("cmd", "/k echo/test_cmdline", "C:\\Users\\Erik\\");
+            //Start the child
+            //I thought about including this step in the constructor but decided I wanted to keep them separate in case more manipulation needs to be done first
             child.Start();
-            //Console.WriteLine("Hello World!");
-            //child.WaitForInputIdle(10);
-            System.Threading.Thread.Sleep(1000);
+            //Get output from the child asynchronously via event handler on the child class
             Console.WriteLine(child.Output);
+            //Send input to the child; include trailing newline
             child.Input.WriteLine("echo/test_file>childtest.txt");
             child.Input.WriteLine("exit");
+            //This is here for cleanup; I like this for sending a command to terminate the child and then waiting for it to exit cleanly
             child.WaitForExit();
         }
     }
