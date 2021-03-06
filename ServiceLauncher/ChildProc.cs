a38d47ab4a79e188b02507c0b32d65224d17ed3b
Aerobreaker <44306739+Aerobreaker@users.noreply.github.com>
2021-03-06 15:55:57 -0700

    Child spawning class
    Created class to spawn child processes and redirect standard streams.  Example usage is in main for testing purposes.

---
+++ ServiceLauncher/ChildProc.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace ServiceLauncher
+{
+    class ChildProc
+    {
+        //Should this be public maybe?  It would simplify the class because anything that's not being manipulated here could be accessed directly
+        private Process _proc { get; set; }
+        public StreamWriter Input { get => _proc.StandardInput; }
+        //StreamReaders are used for synchronous output...which I couldn't figure out anyway
+        //Stringbuilders are used for asynchrounous output.  Better than strings because they're mutable
+        //For some reason, building them as properties with auto generated getters and setters didn't work...
+        public StringBuilder Output = null;
+        public StringBuilder Error = null;
+
+        public ChildProc(string path, string args = "", string workingDir = "")
+        {
+            _proc = new Process();
+
+            _proc.StartInfo = new ProcessStartInfo(path, args)
+            {
+                //Don't create a separate window for the child
+                CreateNoWindow = true,
+                //Don't execute using the shell (so that we can redirect stdout and stderr)
+                UseShellExecute = false,
+                //Don't give an error dialog if the child can't be started
+                ErrorDialog = false,
+                //Redirect standard streams
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                //Start in the provided working directory
+                WorkingDirectory = workingDir
+            };
+
+            //Need to instantiate new stringbuilders
+            Output = new StringBuilder();
+            Error = new StringBuilder();
+
+            //Add our custom event handlers to the output and error data received events for async reading from the streams
+            _proc.OutputDataReceived += ChildOutputHandler;
+            _proc.ErrorDataReceived += ChildErrorHandler;
+        }
+
+        public bool Start()
+        {
+            //So first actually start the process, duh.  Can't be last because the async stream reading has to start after the process starts
+            bool outp = _proc.Start();
+            //Then begin async reading of output and error streams
+            _proc.BeginOutputReadLine();
+            _proc.BeginErrorReadLine();
+            //Return the output from starting the process
+            return outp;
+        }
+
+        //Forward some properties and methods from the process
+        public int Id { get => _proc.Id; }
+        public bool HasExited { get => _proc.HasExited; }
+        public void WaitForExit(int timeout = -1)
+        {
+            _proc.WaitForExit(timeout);
+        }
+        public bool WaitForInputIdle(int timeout = -1)
+        {
+            return _proc.WaitForInputIdle(timeout);
+        }
+
+        //Event handlers for async reading output and error streams
+        private void ChildOutputHandler(object sendingProcess, DataReceivedEventArgs outp)
+        {
+            if (!string.IsNullOrEmpty(outp.Data))
+            {
+                Output.Append(outp.Data);
+            }
+        }
+        private void ChildErrorHandler(object sendingProcess, DataReceivedEventArgs outp)
+        {
+            if (!string.IsNullOrEmpty(outp.Data))
+            {
+                Error.Append(outp.Data);
+            }
+        }
+    }
+}
---
+++ ServiceLauncher/ServiceLauncher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * Todo:
+ * 1. Open named pipes for foreground communication
+ * 2. Forward input and output from child using named pipes
+ * 3. Use pid of child to detect opened ports (optional)
+ * 4. Open ports via upnp (either via step 5 or input parameters)
+ */
+
+namespace ServiceLauncher
+{
+    class ServiceLauncher
+    {
+        static void Main(string[] args)
+        {
+            ChildProc child = new ChildProc("cmd", "/k echo/test_cmdline", "C:\\Users\\Erik\\");
+            child.Start();
+            //Console.WriteLine("Hello World!");
+            //child.WaitForInputIdle(10);
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine(child.Output);
+            child.Input.WriteLine("echo/test_file>childtest.txt");
+            child.Input.WriteLine("exit");
+            child.WaitForExit();
+        }
+    }
+}
