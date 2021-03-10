3ada5eaf263a0689f47122263902ba57023a793c
Aerobreaker <44306739+Aerobreaker@users.noreply.github.com>
2021-03-10 14:43:06 -0700

    Tweak named pipes, update todo
    Added some code related to named pipes, share them across methods, updated todo list, removed now extraneous LauncherControl project (it'll now be handled by the same .exe)

--- LauncherService/LauncherService.cs
+++ LauncherService/LauncherService.cs
@@ -15,18 +15,24 @@ namespace LauncherService
     public partial class LauncherService : ServiceBase
     {
         private int _eventID = 1;
+        public string InstanceName = "";
+        public NamedPipeServerStream OPStream;
+        public NamedPipeServerStream IPStream;
 
         public LauncherService(string name="")
         {
             //Runs once when the service is started the first time
             InitializeComponent();
             /* Todo:
-             * 1. Take command line parameter for installer to set service name; use TransactedInstaller to install and uninstall?
-             * 2. Take command line parameter on service for the name
-             * 3. Store startup and shutdown commands, either in registry or ini file, based on service name
-             * 4. Include the code to start the child process
-             * 5. Create named pipes for interprocess communication
-             * 6. Get rid of launchercontrol?  Should be able to detect between runnig as service and running interactively, using Environment.UserInteractive
+             * 1. Create callback functions for async connections and reads
+             * 2. Change the connection waits to use BeginWaitForConnection
+             * 3. In LauncherControl.cs, create callback function async reads
+             * 4. Figure out how to read and write using the pipes
+             * 5. Store startup and shutdown commands, either in registry or ini file, based on service name
+             * 6. Include the code to start the child process
+             * 7. Use named pipes to facilitate comms with ChildProc
+             * 8. Use pid of ChildProc to detect open ports (optional)
+             * 9. Open ports via UPnP (either via open ports on child pid or via input parameters) (see Device Host API)
              */
 
             //Turn off logging into the application logs
@@ -42,13 +48,14 @@ namespace LauncherService
         {
             //Runs every time the service starts
             string name = args[0];
+            this.InstanceName = name;
             this.WriteLog("Launcher service starting, servicename: " + this.ServiceName + ", name: " + name);
 
-            NamedPipeServerStream opstream = new NamedPipeServerStream("LauncherService" + name + "_output", PipeDirection.Out, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
-            NamedPipeServerStream ipstream = new NamedPipeServerStream("LauncherService" + name + "_input", PipeDirection.In, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+            this.OPStream = new NamedPipeServerStream("LauncherService" + name + "_output", PipeDirection.Out, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+            this.IPStream = new NamedPipeServerStream("LauncherService" + name + "_input", PipeDirection.In, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
 
-            opstream.WaitForConnectionAsync();
-            ipstream.WaitForConnectionAsync();
+            this.OPStream.WaitForConnectionAsync();
+            this.IPStream.WaitForConnectionAsync();
 
             //Next, create ChildProc and loop somehow to link ChildProc streams to ipstream and opstream
             //Child process and command line args, plus startup commands will probably be taken from an ini file in %localappdata%\LauncherService\{name}
@@ -57,7 +64,9 @@ namespace LauncherService
         protected override void OnStop()
         {
             //Runs every time the service stops
-            this.WriteLog("Launcher service stopping");
+            this.WriteLog("Launcher service stopping, servicename: " + this.ServiceName + ", name: " + this.InstanceName);
+            this.OPStream.Close();
+            this.IPStream.Close();
             //Stop the ChildProc here
             //Take commands via...registry?
         }
